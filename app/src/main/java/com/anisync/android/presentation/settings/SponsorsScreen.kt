3d6d179102f76a36272b87b2c69bb350e2fce0d7
Mohammed.Aldakhil <Mohammed.Aldakhil@ebkherne.onmicrosoft.com>
2026-05-16 23:08:47 +0200

    chore(sponsors): drop unused Shape import

--- app/src/main/java/com/anisync/android/presentation/settings/SponsorsScreen.kt
+++ app/src/main/java/com/anisync/android/presentation/settings/SponsorsScreen.kt
@@ -43,7 +43,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
